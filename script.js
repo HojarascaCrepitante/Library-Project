675ee4bfe15fe4e279439bdf42d0c70106ff76bc
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-05-18 17:31:16 -0300

    switched to a table display because why not

--- script.js
+++ script.js
@@ -13,11 +13,42 @@ myLibrary.push(book1,book2,book3)
     let myBooks = document.querySelector('.books')
         function display(){
            myLibrary.forEach(book => {
-            let txt = Object.values(book).join(' ');
-            let card = document.createElement('div')
-            card.classList.add('card')
-            card.textContent= txt;
-            myBooks.appendChild(card)         
+            let bookCard = document.createElement('div');
+            let table = document.createElement('table');
+            let thead = document.createElement('thead');
+            let tbody = document.createElement('tbody');
+            bookCard.appendChild(table);
+            table.appendChild(thead);
+            table.appendChild(tbody);
+            myBooks.appendChild(bookCard);
+            let row1 = document.createElement('tr');
+                let heading1 = document.createElement('th');
+                    heading1.textContent = "Title";
+                let heading2 = document.createElement('th');
+                    heading2.textContent = "Author";
+                let heading3 = document.createElement('th');
+                    heading3.textContent = "Pages";
+                let heading4 = document.createElement('th');
+                    heading4.textContent = "Status";
+                row1.appendChild(heading1);
+                row1.appendChild(heading2);
+                row1.appendChild(heading3);
+                row1.appendChild(heading4);
+                thead.appendChild(row1);
+            let row2 = document.createElement('tr');
+                let row2_data1 = document.createElement('td');
+                    row2_data1.textContent = book.title;
+                let row2_data2 = document.createElement('td');
+                    row2_data2.textContent = book.author;
+                let row2_data3 = document.createElement('td');
+                    row2_data3.textContent = book.pages;
+                let row2_data4 = document.createElement('td');
+                    row2_data4.textContent = book.status;
+                row2.appendChild(row2_data1);
+                row2.appendChild(row2_data2);
+                row2.appendChild(row2_data3);
+                row2.appendChild(row2_data4);
+                tbody.appendChild(row2);
            });
         
         }
